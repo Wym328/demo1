8661b791e2fb121e1dbefa3e2d4a4bb6d9a2d593
root <wym2000328@163.com>
2021-07-29 18:09:14 +0800

    第一次修改

--- src/main/java/com/example/controller/CommentController.java
+++ src/main/java/com/example/controller/CommentController.java
@@ -1,4 +1,13 @@
 package com.example.controller;
 
+import com.example.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class CommentController {
+    @Autowired
+    CommentService commentService;
+    //是是是是
+
 }
